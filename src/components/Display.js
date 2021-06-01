bd6cf734ad8f59bb51667ef1f3a7e7321b6cf0b0
nonuabi <nonuabi@gmail.com>
2021-06-01 21:45:45 +0530

    home component added

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-
+import Home from "./Home";
 class Display extends Component {
   render() {
     return (
       <div className="display">
-        <div className="display_contant"></div>
+        <div className="display_contant">
+          <Home />
+        </div>
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h2>iPod.js</h2>
+        <ul>
+          <li>Control Flow </li>
+          <li>Music </li>
+          <li>Game </li>
+          <li>Setting </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Home;
