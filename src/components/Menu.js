1e919d52638ec299935327cbbb9e792047df3c1d
nonuabi <nonuabi@gmail.com>
2021-06-01 14:34:52 +0530

    menu component is created and styled

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
-
+import { FaFastForward, FaFastBackward, FaPause } from "react-icons/fa";
 class Menu extends Component {
   render() {
     return (
       <div className="menu">
         <div className="circle">
           <div className="center_btn"></div>
-          <div className="menu_btn"></div>
-          <div className="next_btn"></div>
-          <div className="prev_btn">
-            <img></img>
+          <div className="menu_btn">
+            <button>MENU</button>
           </div>
-          <div className="pause_btn"></div>
+          <div className="next_btn">
+            <button>
+              <FaFastForward size="20px" />
+            </button>
+          </div>
+          <div className="prev_btn">
+            <button>
+              <FaFastBackward size="20px" />
+            </button>
+          </div>
+          <div className="pause_btn">
+            <button>
+              <FaPause size="20px" />
+            </button>
+          </div>
         </div>
       </div>
     );
