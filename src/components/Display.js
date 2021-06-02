96bc07672d762b9296307c2b80fbf9343430514a
nonuabi <nonuabi@gmail.com>
2021-06-02 13:34:16 +0530

    added Setting component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import Home from "./Home";
 import Music from "./Music";
-
+import Game from "./Game";
+import Setting from "./Setting";
 class Display extends Component {
   render() {
     return (
       <div className="display">
         <div className="display_contant">
+          {/* <Game /> */}
           <Home />
           {/* <Music /> */}
+          {/* <Setting /> */}
         </div>
       </div>
     );
---
+++ src/components/Setting.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Setting extends Component {
+  render() {
+    return (
+      <div className="setting display_default">
+        <h2>Settings</h2>
+        <ul>
+          <li>ipod themes</li>
+          <li>Wheel Color</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Setting;
