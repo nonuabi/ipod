64e1e13da58df6b68002670fbc825ef4b494fa09
nonuabi <nonuabi@gmail.com>
2021-06-02 23:20:32 +0530

    touch feature is added to the music component

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,11 @@ class App extends React.Component {
       index: -1,
     };
   }
+  componentDidMount() {
+    let inital = document.getElementById("0");
+    inital.style.backgroundColor = "#2B81A3";
+    inital.style.color = "white";
+  }
 
   rotateWheel = () => {
     let angle = 0;
@@ -21,154 +26,104 @@ class App extends React.Component {
     const region = new ZingTouch.Region(target);
     region.bind(target, "rotate", (e) => {
       angle = angle + e.detail.distanceFromLast;
-      switch (this.state.currentState) {
-        case 1:
-          if (angle < 0) {
-            let coverFlow = document.getElementById("0");
-            coverFlow.style.backgroundColor = "#2B81A3";
-            coverFlow.style.color = "white";
 
-            this.setState({
-              index: 0,
-            });
+      if (angle < 0) {
+        let coverFlow = document.getElementById("0");
+        coverFlow.style.backgroundColor = "#2B81A3";
+        coverFlow.style.color = "white";
 
-            let music = document.getElementById("1");
-            music.style.backgroundColor = "white";
-            music.style.color = "black";
+        this.setState({
+          index: 0,
+        });
 
-            let games = document.getElementById("2");
-            games.style.color = "black";
-            games.style.backgroundColor = "white";
+        let music = document.getElementById("1");
+        music.style.backgroundColor = "white";
+        music.style.color = "black";
 
-            let settings = document.getElementById("3");
-            settings.style.color = "black";
-            settings.style.backgroundColor = "white";
-          }
-          if (angle > 0 && angle < 15) {
-            let coverFlow = document.getElementById("0");
-            coverFlow.style.backgroundColor = "white";
-            coverFlow.style.color = "black";
+        if (this.state.currentState === 1 || this.state.currentState === 3) {
+          let games = document.getElementById("2");
+          games.style.color = "black";
+          games.style.backgroundColor = "white";
+        }
 
-            let music = document.getElementById("1");
-            music.style.backgroundColor = "#2B81A3";
-            music.style.color = "white";
+        if (this.state.currentState === 1) {
+          let settings = document.getElementById("3");
+          settings.style.color = "black";
+          settings.style.backgroundColor = "white";
+        }
+      }
+      if (angle > 0 && angle < 15) {
+        let coverFlow = document.getElementById("0");
+        coverFlow.style.backgroundColor = "white";
+        coverFlow.style.color = "black";
 
-            this.setState({
-              index: 1,
-            });
+        let music = document.getElementById("1");
+        music.style.backgroundColor = "#2B81A3";
+        music.style.color = "white";
 
-            let games = document.getElementById("2");
-            games.style.color = "black";
-            games.style.backgroundColor = "white";
+        this.setState({
+          index: 1,
+        });
 
-            let settings = document.getElementById("3");
-            settings.style.color = "black";
-            settings.style.backgroundColor = "white";
-          }
-          if (angle > 15 && angle < 30) {
-            let coverFlow = document.getElementById("0");
-            coverFlow.style.backgroundColor = "white";
-            coverFlow.style.color = "black";
+        if (this.state.currentState === 1 || this.state.currentState === 3) {
+          let games = document.getElementById("2");
+          games.style.color = "black";
+          games.style.backgroundColor = "white";
+        }
 
-            let music = document.getElementById("1");
-            music.style.backgroundColor = "white";
-            music.style.color = "black";
+        if (this.state.currentState === 1) {
+          let settings = document.getElementById("3");
+          settings.style.color = "black";
+          settings.style.backgroundColor = "white";
+        }
+      }
+      if (
+        angle > 15 &&
+        angle < 30 &&
+        (this.state.currentState === 1 || this.state.currentState === 3)
+      ) {
+        let coverFlow = document.getElementById("0");
+        coverFlow.style.backgroundColor = "white";
+        coverFlow.style.color = "black";
 
-            let games = document.getElementById("2");
-            games.style.color = "white";
-            games.style.backgroundColor = "#2B81A3";
+        let music = document.getElementById("1");
+        music.style.backgroundColor = "white";
+        music.style.color = "black";
 
-            this.setState({
-              index: 2,
-            });
+        let games = document.getElementById("2");
+        games.style.color = "white";
+        games.style.backgroundColor = "#2B81A3";
 
-            let settings = document.getElementById("3");
-            settings.style.color = "black";
-            settings.style.backgroundColor = "white";
-          }
-          if (angle > 30 && angle < 45) {
-            let coverFlow = document.getElementById("0");
-            coverFlow.style.backgroundColor = "white";
-            coverFlow.style.color = "black";
+        this.setState({
+          index: 2,
+        });
 
-            let music = document.getElementById("1");
-            music.style.backgroundColor = "white";
-            music.style.color = "black";
+        if (this.state.currentState === 1) {
+          let settings = document.getElementById("3");
+          settings.style.color = "black";
+          settings.style.backgroundColor = "white";
+        }
+      }
+      if (angle > 30 && angle < 45 && this.state.currentState === 1) {
+        let coverFlow = document.getElementById("0");
+        coverFlow.style.backgroundColor = "white";
+        coverFlow.style.color = "black";
 
-            let games = document.getElementById("2");
-            games.style.color = "black";
-            games.style.backgroundColor = "white";
+        let music = document.getElementById("1");
+        music.style.backgroundColor = "white";
+        music.style.color = "black";
 
-            let settings = document.getElementById("3");
-            settings.style.color = "white";
-            settings.style.backgroundColor = "#2B81A3";
+        let games = document.getElementById("2");
+        games.style.color = "black";
+        games.style.backgroundColor = "white";
 
-            this.setState({
-              index: 3,
-            });
-          }
-          break;
-        //   break;
-        // case 2:
-        //   break;
-        // case 3:
-        //   // if (angle < 0) {
-        //   //   let allMusic = document.getElementById("0");
-        //   //   allMusic.style.backgroundColor = "#2B81A3";
-        //   //   allMusic.style.color = "white";
-        //   //   this.setState({
-        //   //     index: 0,
-        //   //   });
+        let settings = document.getElementById("3");
+        settings.style.color = "white";
+        settings.style.backgroundColor = "#2B81A3";
 
-        //   //   let artices = document.getElementById("1");
-        //   //   artices.style.backgroundColor = "white";
-        //   //   artices.style.color = "black";
-
-        //   //   let album = document.getElementById("2");
-        //   //   album.style.color = "black";
-        //   //   album.style.backgroundColor = "white";
-        //   // }
-        //   // if (angle > 0 && angle < 15) {
-        //   //   let allMusic = document.getElementById("0");
-        //   //   allMusic.style.backgroundColor = "white";
-        //   //   allMusic.style.color = "black";
-
-        //   //   let artices = document.getElementById("1");
-        //   //   artices.style.backgroundColor = "#2B81A3";
-        //   //   artices.style.color = "white";
-
-        //   //   this.setState({
-        //   //     index: 1,
-        //   //   });
-
-        //   //   let album = document.getElementById("2");
-        //   //   album.style.color = "black";
-        //   //   album.style.backgroundColor = "white";
-        //   // }
-        //   // if (angle > 15 && angle < 30) {
-        //   //   let allMusic = document.getElementById("0");
-        //   //   allMusic.style.backgroundColor = "white";
-        //   //   allMusic.style.color = "black";
-
-        //   //   let artices = document.getElementById("1");
-        //   //   artices.style.backgroundColor = "white";
-        //   //   artices.style.color = "black";
-
-        //   //   let album = document.getElementById("2");
-        //   //   album.style.color = "white";
-        //   //   album.style.backgroundColor = "#2B81A3";
-
-        //   //   this.setState({
-        //   //     index: 2,
-        //   //   });
-        //   }
-        //   break;
-        // case 4:
-        //   break;
-        // case 5:
-        //   break;
-        default:
-          break;
+        this.setState({
+          index: 3,
+        });
       }
     });
   };
@@ -185,12 +140,10 @@ class App extends React.Component {
           currentState: 2,
           prevState: 1,
         });
-
         break;
       case 1:
         this.setState({
           index: -1,
-
           currentState: 3,
           prevState: 1,
         });
@@ -198,7 +151,6 @@ class App extends React.Component {
       case 2:
         this.setState({
           index: -1,
-
           currentState: 4,
           prevState: 1,
         });
@@ -206,11 +158,9 @@ class App extends React.Component {
       case 3:
         this.setState({
           index: -1,
-
           currentState: 5,
           prevState: 1,
         });
-
         break;
       default:
         this.setState({
@@ -223,7 +173,6 @@ class App extends React.Component {
     console.log("prevState   ", this.state.prevState);
   };
   backwardMove = () => {
-    // let prevState = this.state.currentState;
     const { prevState } = this.state;
     if (this.state.currentState === 1) {
       return;
